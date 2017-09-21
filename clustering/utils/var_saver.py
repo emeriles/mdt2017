f2f87a36b465d437bd623b6086ee0662c1ac0279
Emanuel Meriles <emeriles@gmail.com>
2017-09-21 11:22:54 -0300

    renameing files

--- clustering/utils/var_saver.py
+++ clustering/utils/var_saver.py
@@ -1,24 +1,23 @@
 import pickle
-import numpy as np
 
 def data_to_file(data, filepath):
+    """
+    Save a value of a variable data to filepath
+
+    Usage:
+    data_to_file(data, filepath)
+    """
     with open(filepath, "wb") as fp:   #Pickling
         result = pickle.dump(data, fp)
     return result
 
 def data_from_file(filepath):
+    """
+    Loads the data from filepath to a variable var
+
+    Usage:
+    var = data_from_file(filepath)
+    """
     with open(filepath, "rb") as fp:   # Unpickling
         b = pickle.load(fp)
     return b
-
-arr = np.array(range(0,10000))
-
-res = data_to_file(arr, "./PICKLERICK.pickle")
-
-print(res)
-
-print("---")
-
-arr2 = data_from_file("./PICKLERICK.pickle")
-
-print(type(arr2))
