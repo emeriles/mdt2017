dcedbd02eb89a3ab5f6ad3c30a063a0492b1434f
Emanuel Meriles <emeriles@gmail.com>
2017-09-22 03:13:55 -0300

    vectorizer: now counting enviroment occurrences

--- clustering/vectorizer.py
+++ clustering/vectorizer.py
@@ -28,6 +28,7 @@ class Vectorizer():
         self.corpus = corpus
         self.vec_type = vec_type
         self.pos_tagger = POS_Tagger(pos_tagger)
+        self.words = None
 
     def __tag_combinations(self, rawtag):
         """
@@ -38,7 +39,12 @@ class Vectorizer():
         return [letter_tag[:i] for i in range(1, len(letter_tag)+1)]
 
     def get_vector_matrix(self):
-        # dict of features
+        def _update_pos_t_feature():
+            f_val = 1
+            if word in vectors:
+                if feature_name in vectors[word]:
+                    f_val = vectors[word][feature_name] + 1 # if seen before!
+            features[feature_name] = f_val
 
         sents = self.corpus.get_sents()
         tagged_sents = self.pos_tagger.get_tagged_sents(sents)
@@ -58,10 +64,11 @@ class Vectorizer():
                 if word_idx > 0:
                     prev_tag = sent[word_idx-1][1]
                     feature_name = prev_tag + '_prev'
-                    features[feature_name] = 1        # si ya existe, contar + 1 !!
+                    _update_pos_t_feature()
                 if word_idx < len(sent)-1:
                     post_tag = sent[word_idx+1][1]
-                    features[post_tag + '_post'] = 1
+                    feature_name = post_tag + '_post'
+                    _update_pos_t_feature()
 
                 # frequency counting
                 if (word in vectors):
@@ -75,5 +82,6 @@ class Vectorizer():
         self.words = list(vectors.keys())          # thankfully in the same order as vectors.values
 
         vectorizer = DictVectorizer(dtype=numpy.int32)
-        self.vec_matrix = vectorizer.fit_transform(list(vectors.values()))
-        return self.words, self.vec_matrix
+        vec_matrix = vectorizer.fit_transform(list(vectors.values()))
+        #print (vectorizer.inverse_transform(vec_matrix)[:10]) # -> to see features!
+        return self.words, vec_matrix
